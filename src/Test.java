b442a08f3f8fd63ceb30dba1c1e3c3f484c0888f
boris liu <boris liu>
2016-08-04 16:07:29 +0800

    add log4j
    Change-Id: I03846ee6c56379d926df0995b9af11800bcb9012

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,9 @@ import com.manager.CWManager;
 
 
 public class Test {
-
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub  
-		CWManager.getInstance().contorlAndGetData("07600000", "D://", 5, 5);
+		CWManager.getInstance().contorlAndGetData("07600000", "D://", 60, 60);
 	}
 
 }
